988da3876684c4d40c4ed85f900fab0c60faebfd
Orane31 <orane.chaper@gmail.com>
2020-07-21 12:15:24 +0200

    version 1 done

--- app.rb
+++ app.rb
@@ -11,19 +11,34 @@ player2 = Player.new("José")
 def intro(player1, player2)
     player1.show_state
     player2.show_state
+end
 
+def start
+    puts "Let's play"
+end
+
+def fight(player1, player2)
+    while player1.life_points > 0 || player2.life_points > 0
+    player1.attacks(player2)
+    puts "\n"
+    if player2.life_points <= 0 
+        break
+    end
+    player2.attacks(player1)
+    end
 end
 
 
 
+def playin(player1, player2)
 
+        intro(player1, player2) 
+        start
+        fight(player1, player2)
 
-p intro(player2, player1)
+end
 
-
-
-
-
+p playin(player1, player2)
 
 
 
--- lib/player.rb
+++ lib/player.rb
@@ -8,25 +8,26 @@ class Player
 
 # Afficher l'état de l'objet Player sur laquelle elle est exécutée : "XXXX a YYY points de vie".
     def show_state
-        puts "#{@name} has #{@life_points} left !"
+        return "#{@name} has #{@life_points} left !"
     end
 
 # Le joueur stocké dans l'objet player1 subit 5 pts de dommage en faisant un player1.gets_damage(5).
-    def gets_damage(damage_pts)
-        @damage_pts = damage_pts
-        @life_points = @life_points - @damage_pts
+    def gets_damage(pts_taken)
+        @pts_taken = pts_taken
+        @life_points = @life_points - @pts_taken
         if @life_points <= 0
             puts "#{@name} is dead !"
-        @life_points = 0
+            @life_points = 0
         end
+        return @life_points
     end
 
 # Pour que le player1 attaque le player2, on aura juste à taper player1.attacks(player2).
     def attacks(attacked)
         puts "Player #{self.name} is attacking player #{attacked.name} !"
-        attack = compute_damage
-        attacked.gets_damage(attack)
-        puts "#{attacked.name} lost #{attack} life points !"
+        pts_taken = compute_damage
+        puts "#{attacked.name} lost #{pts_taken} life points ! \n"
+        attacked.gets_damage(pts_taken)
     end
 
     def compute_damage
